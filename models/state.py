f3f4b90b45c334058e6b9054c46acc9608912022
Zouhair OB <46304063+ZooHairDev@users.noreply.github.com>
2023-11-17 13:39:00 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import models
+from models.city import City
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",cascade="all, delete, delete-orphan")
+
+    @property
+    def my_cities(self):
+        """list of city instances"""
+        from models import storage
+        city_list = []
+            for city in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
